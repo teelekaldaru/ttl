53580cae50909bb9ef8fdf828807a64510056e70
HGRUPP\teele.kaldaru <teele.kaldaru@helmes.ee>
2023-04-15 07:30:38 +0300

    Add register form link

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,24 +7,11 @@
 
         <div class="row pb-5">
           <div class="col">
-            <h5 class="page-section-content py-3">
-              Tamsalu Tantsulaager 2022 REGISTREERIMINE toimub 1. maist - 30. maini!
+            <h5 class="page-section-content py-4">
+              Tamsalu Tantsulaager 2023 REGISTREERIMINE toimub 1. maist - 30. maini!
             </h5>
-            <button type="button" class="btn btn-primary">REGISTREERI TANTSIJAKS <i class="fa fa-arrow-right ml-1"></i></button>
+            <button type="button" class="btn btn-lg btn-primary" @click="openForm()">REGISTREERI TANTSIJAKS </button>
           </div>
-        </div>
-
-        <div class="row">
-            <div class="col">
-              <h5 class="page-section-content py-3">
-                Sel aastal on peale tantsijate meil ka lavakujunduse rühm 4. - 12. klassi noortele! (Kohti selles rühmas on 10)<br />
-                Lavakujunduse rühm tegeleb juhendaja eestvedamisel laagris erinevate loovate tegevustega (meisterdamine, maalimine jms),
-                osaleb laagri vältel põnevates loovates töötubades ning vastutab kontserdi lavakujunduse ning dekoratsioonide valmimise eest. 
-                Kui tunned, et soovid laagris osaleda, aga ei ole kõige pädevam tantsulõvi,
-                vaid pigem terava silma ja loovate ideedega, siis nüüd saad selleks võimaluse!
-              </h5>
-              <button type="button" class="btn btn-primary">REGISTREERI LAVAKUJUNDAJAKS <i class="fa fa-arrow-right ml-1"></i></button>
-            </div>
         </div>
       </div>
   </section>
@@ -36,5 +23,10 @@ import { Options, Vue } from "vue-class-component";
 @Options({
   components: { },
 })
-export default class Register extends Vue {}
+export default class Register extends Vue {
+
+  openForm() {
+    window.open("https://forms.gle/34z4WooyeoTpwgX58");
+  }
+}
 </script>
